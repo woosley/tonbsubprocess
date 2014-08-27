c677357de925264086eb2868fb13e49190d25e94
woosley <woosley.xu@demonware.net>
2014-08-27 16:56:25 +0800

    first commit

---
+++ nonblockingsubprocess.py
@@ -0,0 +1,106 @@
+""" NonBlockingSubprocess: the nonblocking subprocess for tornado.
+Usage:
+    res = yield gen.Task(NonBlockingSubprocess(ioloop, cmd).loop)
+    result = res.gather_response()
+    print result['data']
+    print result['success']
+
+Those attributes are saved for later usages:
+    cmd: command line
+    data: command output
+    start_time: start timestamp of the command
+    end_time: end timestamp of the command
+    pipe: the subprocess.Popen() object, which can be used for getting the
+    return code of the command
+
+You can also inherit from class NonBlockingSubprocess and write your own
+function log_data, it is called when reading data from subprocess output
+
+    def log_data(self, data):
+        print data
+
+"""
+
+import subprocess
+import fcntl
+import os
+import functools
+import time
+from tornado.ioloop import IOLoop
+
+
+class NonBlockingSubprocess(object):
+    """A non blocking subprocess handler"""
+
+    def __init__(self, cmd):
+
+        self.start_time = time.time()
+        self.pipe = subprocess.Popen(cmd,
+                                     shell=True,
+                                     stdin=subprocess.PIPE,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.STDOUT,
+                                     close_fds=True)
+        self.cmd = cmd
+        self.ioloop = IOLoop.instance()
+        self.data = ""
+
+    def loop(self, callback=None):
+
+        # make the filehandlers non-blocking
+        # do not use stdout.fileno() here
+        fh = self.pipe.stdout
+        fl = fcntl.fcntl(fh, fcntl.F_GETFL)
+        fcntl.fcntl(fh, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+        self.fh = fh
+
+        # you must have a callback here
+        fd_callback = functools.partial(self.__poll_handler, callback=callback)
+        self.ioloop.add_handler(self.fh, fd_callback, self.ioloop.READ)
+
+    def __poll_handler(self, fh, event, callback=None):
+        fd = fh.fileno()
+        if self.pipe.poll() is not None:
+            # event finished
+            self.ioloop.remove_handler(fd)
+            data = self.pipe.stdout.read()
+            if len(data) > 0:
+                self.log_data(data)
+                self.data += data
+            self.end_time = time.time()
+
+            # and you have to set the callback here!
+            # return the class for later usage
+            callback(self)
+
+        data = ""
+        while True:
+            try:
+                data = os.read(fd, 512)
+                if len(data) == 0:
+                    # command finished
+                    break
+                else:
+                    self.data += data
+                    self.log_data(data)
+            # OSError: [Errno 11] Resource temporarily unavailable
+            except OSError as e:
+                break
+
+    def log_data(self, data):
+        """ used for logging cmd output in realtime. """
+        pass
+
+    def gather_response(self):
+        """ gather response from NonBlockingSubprocess object. """
+
+        res = {}
+        res['returncode'] = self.pipe.returncode
+        res['cmd'] = self.cmd
+        res['data'] = self.data
+        res['start_time'] = self.start_time
+        res['end_time'] = self.end_time
+        res['success'] = True if res['returncode'] == 0 else False
+        res['pid'] = self.pipe.pid
+        res['reason'] = "command returned {}".format(res['returncode'])
+        return res
---
+++ test.py
@@ -0,0 +1,38 @@
+import time
+from tornado import gen
+from tornado.testing import AsyncTestCase, gen_test
+from tornado.ioloop import IOLoop
+from nonblockingsubprocess import NonBlockingSubprocess
+
+
+class TestSubprocess(NonBlockingSubprocess):
+    def __init__(self, cmd):
+        self.datalogging = []
+        super(TestSubprocess, self).__init__(cmd)
+
+    def log_data(self, data):
+        self.datalogging.append([time.time(), data])
+
+
+class TestPandaSubprocess(AsyncTestCase):
+
+    @gen_test(timeout=10)
+    def test_command(self):
+        cmd = "echo 1 && sleep 1 && echo 2"
+        resobj = yield gen.Task(TestSubprocess(cmd).loop)
+        self.assertTrue(len(resobj.datalogging) == 2)
+
+    def get_new_ioloop(self):
+        return IOLoop.instance()
+
+    @gen_test(timeout=10)
+    def test_multi_commands(self):
+        cmds = ['echo 1 && sleep 1 && echo 1.1',
+                'echo 2 && sleep 2 && echo 2.2',
+                'echo 3 && sleep 3 && echo 3.3']
+        gens = [gen.Task(TestSubprocess(i).loop)
+                for i in cmds]
+        resobjs = yield gens
+        self.assertEqual(len(resobjs), 3)
+        self.assertEqual(int(resobjs[1].datalogging[-1][0] - resobjs[0].datalogging[-1][0]), 1)
+        self.assertEqual(int(resobjs[2].datalogging[-1][0] - resobjs[0].datalogging[-1][0]), 2)
